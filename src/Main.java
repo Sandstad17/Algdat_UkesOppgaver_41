4de8001d18cdd5cab590ac66050d7138ff159925
Sandstad17 <90138783+Sandstad17@users.noreply.github.com>
2022-10-14 21:54:53 +0200

    Lagt til en oppgave som må gjøres

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-       ///highlight ??
-
+       ///finn ut av hva som ligger øverst etter stakken er fullført
+        A.leggInn(2);
+        A.leggInn(5);
+        A.leggInn(1);
+        A.kikk();
+        A.taUt()
     }
 }
